ebf464fc87eeb2c72a5e9a613d155435e5be6dd7
Rafael Augusto <rafael.augusto.fm@outlook.com>
2018-05-11 18:37:21 -0300

    Fixing insertion of sample data on startup for development use with MySQL

--- src/main/java/com/daitangroup/initproj/Application.java
+++ src/main/java/com/daitangroup/initproj/Application.java
@@ -1,12 +1,27 @@
 package com.daitangroup.initproj;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+import com.daitangroup.initproj.model.Person;
+import com.daitangroup.initproj.repository.PersonRepository;
 
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	@Autowired private PersonRepository personRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		personRepository.save(new Person("Joaquim", "BR"));
+		personRepository.save(new Person("Bran", "US"));
+		personRepository.save(new Person("Yushin", "JP"));
+		personRepository.save(new Person("Pablo", "ES"));
 	}
 }
--- src/main/java/com/daitangroup/initproj/model/Person.java
+++ src/main/java/com/daitangroup/initproj/model/Person.java
@@ -16,6 +16,13 @@ public class Person {
 	private String name;
 	
 	private String country;
+	
+	public Person() {}
+
+	public Person(String name, String country) {
+		this.name = name;
+		this.country = country;
+	}
 
 	public Integer getId() {
 		return id;
