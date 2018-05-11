4b03959e46242fef434392906e710f4e17bf6798
Rafael Augusto <rafael.augusto.fm@outlook.com>
2018-05-11 13:52:40 -0300

    Annotating repository classes with Spring's @Repository

--- src/main/java/com/daitangroup/initproj/repository/PersonRepository.java
+++ src/main/java/com/daitangroup/initproj/repository/PersonRepository.java
@@ -1,7 +1,9 @@
 package com.daitangroup.initproj.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.daitangroup.initproj.model.Person;
 
+@Repository
 public interface PersonRepository extends CrudRepository<Person, Integer> {}
