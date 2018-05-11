bf75458ad98bd70ba868bf45cb554c1a9ec40953
Rafael Augusto <rafael.augusto.fm@outlook.com>
2018-05-11 14:37:49 -0300

    Implementing sorting and pagination in Person's REST endpoint

--- src/main/java/com/daitangroup/initproj/repository/PersonRepository.java
+++ src/main/java/com/daitangroup/initproj/repository/PersonRepository.java
@@ -1,9 +1,9 @@
 package com.daitangroup.initproj.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.daitangroup.initproj.model.Person;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Integer> {}
+public interface PersonRepository extends PagingAndSortingRepository<Person, Integer> {}
