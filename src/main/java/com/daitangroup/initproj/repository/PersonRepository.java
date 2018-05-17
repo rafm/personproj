1dfc0dc58500c6e7055c84034ef276abdd9dc262
Rafael Augusto <rafael.augusto.fm@outlook.com>
2018-05-17 15:09:30 -0300

    Changing repository interface to module-specific repository

--- src/main/java/com/daitangroup/initproj/repository/PersonRepository.java
+++ src/main/java/com/daitangroup/initproj/repository/PersonRepository.java
@@ -1,9 +1,9 @@
 package com.daitangroup.initproj.repository;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.daitangroup.initproj.model.Person;
 
 @Repository
-public interface PersonRepository extends PagingAndSortingRepository<Person, Integer> {}
+public interface PersonRepository extends JpaRepository<Person, Integer> {}
